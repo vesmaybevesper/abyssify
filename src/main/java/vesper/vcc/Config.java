8c943f3f186a988eacf2b548e17902f59a2794ef
Ves <vesmaybevesper@gmail.com>
2025-06-05 15:10:52 -0400

    Begin Config.java creation

---
+++ src/main/java/vesper/vcc/Config.java
@@ -0,0 +1,7 @@
+package vesper.vcc;
+
+import eu.midnightdust.lib.config.MidnightConfig;
+
+public class Config extends MidnightConfig {
+        @Entry public static boolean EffectiveXWakes = true;
+}
